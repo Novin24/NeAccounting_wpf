c9029d8c63219b6281f701fe273fde6964ff25b5
AliPayandeh <Poria9573351@gmail.com>
2023-08-17 22:40:38 +0330

    #5

--- Np_Accounting/Np_Accounting/Views/Windows/LogInWindow.xaml.cs
+++ Np_Accounting/Np_Accounting/Views/Windows/LogInWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace Np_Accounting.Views.Windows
             ViewModel = logInViewModel;
             DataContext = this;
             _serviceProvider = serviceProvider;
+            Txt_UserName.Focus();
         }
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -43,7 +44,7 @@ namespace Np_Accounting.Views.Windows
 
         private async void btnlogin_Click(object sender, RoutedEventArgs e)
         {
-            //if (await ViewModel.LogIn(Txt_UserName.Text, txt_password.Password))
+            if (await ViewModel.LogIn(Txt_UserName.Text, txt_password.Password))
             {
                 Hide();
                 if (!System.Windows.Application.Current.Windows.OfType<MainWindow>().Any())
