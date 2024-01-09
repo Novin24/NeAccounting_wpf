b156f7dd3d1ee37745b25927f2e6b615be6eadf8
7607711 <7607711@gmail.com>
2024-01-09 16:22:35 +0330

    #84

--- NeAccounting/NeAccounting/Views/Pages/Creditors/CreditorsListPage.xaml.cs
+++ NeAccounting/NeAccounting/Views/Pages/Creditors/CreditorsListPage.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace NeAccounting.Views.Pages.Creditors
+namespace NeAccounting.Views.Pages
 {
     /// <summary>
     /// Interaction logic for CreditorsListPage.xaml
