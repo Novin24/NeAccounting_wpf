e4ae5c682ebcc51f22d87f18ca3f8dd29ebdcee5
7607711 <7607711@gmail.com>
2024-02-04 12:46:59 +0330

    #100

---
+++ NeAccounting/NeAccounting/Controls/Header/HeaderPack.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace NeAccounting.Controls
+{
+    /// <summary>
+    /// Interaction logic for HeaderPack.xaml
+    /// </summary>
+    public partial class HeaderPack : UserControl
+    {
+
+        public HeaderPack()
+        {
+            InitializeComponent();
+
+            PersianCalendar persianCalendar = new PersianCalendar();
+            string DateHeader = persianCalendar.GetYear(DateTime.Now) + "/" + persianCalendar.GetMonth(DateTime.Now) + "/" + persianCalendar.GetDayOfMonth(DateTime.Now);
+            txt_date.Text = DateHeader;
+        }
+    }
+}
