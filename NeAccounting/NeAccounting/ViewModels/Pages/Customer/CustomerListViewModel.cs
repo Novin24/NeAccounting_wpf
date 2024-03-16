f68049acbc9780962872674114bc5c6657673015
7607711 <7607711@gmail.com>
2024-03-16 10:29:02 +0330

    #156

--- NeAccounting/NeAccounting/ViewModels/Pages/Customer/CustomerListViewModel.cs
+++ NeAccounting/NeAccounting/ViewModels/Pages/Customer/CustomerListViewModel.cs
@@ -76,32 +76,31 @@ namespace NeAccounting.ViewModels
             _ = _navigationService.Navigate(pageType);
         }
 
-        private async Task OnArchiveCus(Guid parameter, bool isArchive)
-        {
-            var result = await _contentDialogService.ShowSimpleDialogAsync(
-            new SimpleContentDialogCreateOptions()
-            {
-                Title = "آیا از حذف اطمینان دارید!!!",
-                Content = Application.Current.Resources["DeleteDialogContent"],
-                PrimaryButtonText = "بله",
-                SecondaryButtonText = "خیر",
-                CloseButtonText = "انصراف",
-            });
+        //private async Task OnArchiveCus(Guid parameter, bool isArchive)
+        //{
+        //    var result = await _contentDialogService.ShowSimpleDialogAsync(new SimpleContentDialogCreateOptions()
+        //    {
+        //        Title = "آیا از بایگانی اطمینان دارید!!!",
+        //        Content = Application.Current.Resources["DeleteDialogContent"],
+        //        PrimaryButtonText = "بله",
+        //        SecondaryButtonText = "خیر",
+        //        CloseButtonText = "انصراف",
+        //    });
 
-            if (result == ContentDialogResult.Primary)
-            {
-                using UnitOfWork db = new();
-                var (e, s) = await db.CustomerManager.ArchiveCustomer(parameter, isArchive);
-                if (!s)
-                {
-                    _snackbarService.Show("کاربر گرامی", e, ControlAppearance.Secondary, new SymbolIcon(SymbolRegular.Warning20, new SolidColorBrush(Colors.Goldenrod)), TimeSpan.FromMilliseconds(3000));
-                    return;
-                }
-                _snackbarService.Show("کاربر گرامی", "عملیات با موفقیت انجام شد.", ControlAppearance.Success, new SymbolIcon(SymbolRegular.CheckmarkCircle20), TimeSpan.FromMilliseconds(3000));
+        //    if (result == ContentDialogResult.Primary)
+        //    {
+        //        using UnitOfWork db = new();
+        //        var (e, s) = await db.CustomerManager.ArchiveCustomer(parameter, isArchive);
+        //        if (!s)
+        //        {
+        //            _snackbarService.Show("کاربر گرامی", e, ControlAppearance.Secondary, new SymbolIcon(SymbolRegular.Warning20, new SolidColorBrush(Colors.Goldenrod)), TimeSpan.FromMilliseconds(3000));
+        //            return;
+        //        }
+        //        _snackbarService.Show("کاربر گرامی", "عملیات با موفقیت انجام شد.", ControlAppearance.Success, new SymbolIcon(SymbolRegular.CheckmarkCircle20), TimeSpan.FromMilliseconds(3000));
 
-                await OnSearchCus();
-            }
-        }
+        //        await OnSearchCus();
+        //    }
+        //}
 
         [RelayCommand]
         private void OnUpdateCus(Guid parameter)
@@ -156,6 +155,22 @@ namespace NeAccounting.ViewModels
             //});
 
             //servise.Navigate(pageType, context);
+        }
+        [RelayCommand]
+        private async Task OnActive(Guid id)
+        {
+            using UnitOfWork db = new();
+            await db.CustomerManager.ArchiveCustomer(id, true);
+            await db.SaveChangesAsync();
+            List = await db.CustomerManager.GetCustomerList(string.Empty, string.Empty, string.Empty);
+        }
+        [RelayCommand]
+        private async Task OnDeActive(Guid id)
+        {
+            using UnitOfWork db = new();
+            await db.CustomerManager.ArchiveCustomer(id, false);
+            await db.SaveChangesAsync();
+            List = await db.CustomerManager.GetCustomerList(string.Empty, string.Empty, string.Empty);
         }
     }
 }
--- NeAccounting/NeAccounting/Views/Pages/Customers/CustomerListPage.xaml.cs
+++ NeAccounting/NeAccounting/Views/Pages/Customers/CustomerListPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using NeAccounting.ViewModels;
+using Wpf.Ui;
 using Wpf.Ui.Controls;
 
 namespace NeAccounting.Views.Pages
@@ -8,13 +9,61 @@ namespace NeAccounting.Views.Pages
     /// </summary>
     public partial class CustomerListPage : INavigableView<CustomerListViewModel>
     {
+        private readonly IContentDialogService _contentDialogService;
         public CustomerListViewModel ViewModel { get; }
-        public CustomerListPage(CustomerListViewModel viewModel)
+        public CustomerListPage(IContentDialogService contentDialogService, CustomerListViewModel viewModel)
         {
             ViewModel = viewModel;
+            _contentDialogService = contentDialogService;
             DataContext = this;
             InitializeComponent();
             CusName_txt.Focus();
+        }
+        private void CheckBox_Status_Chkecked(object sender, RoutedEventArgs e)
+        {
+            if (sender is not System.Windows.Controls.CheckBox btn)
+                return;
+
+            if (btn.Tag == null)
+                return;
+
+            Guid id = Guid.Parse(btn.Tag.ToString());
+            var unit = ViewModel.List.First(x => x.Id == id);
+            if (unit.IsActive)
+            {
+                return;
+            }
+            ViewModel.ActiveCommand.ExecuteAsync(id);
+        }
+        private async void CheckBox_Status_Unckecked(object sender, RoutedEventArgs e)
+        {
+
+            if (sender is not System.Windows.Controls.CheckBox chb)
+                return;
+            chb.IsChecked = true;
+
+            if (chb.Tag == null)
+                return;
+
+            Guid id = Guid.Parse(chb.Tag.ToString());
+            var unit = ViewModel.List.First(x => x.Id == id);
+            if (!unit.IsActive)
+            {
+                return;
+            }
+            var result = await _contentDialogService.ShowSimpleDialogAsync(new SimpleContentDialogCreateOptions()
+            {
+                Title = "آیا از بایگانی اطمینان دارید!!!",
+                Content = Application.Current.Resources["DeleteDialogContent"],
+                PrimaryButtonText = "بله",
+                SecondaryButtonText = "خیر",
+                CloseButtonText = "انصراف",
+            });
+
+            if (result == ContentDialogResult.Primary)
+            {
+                await ViewModel.DeActiveCommand.ExecuteAsync(id);
+            }
         }
     }
 }
