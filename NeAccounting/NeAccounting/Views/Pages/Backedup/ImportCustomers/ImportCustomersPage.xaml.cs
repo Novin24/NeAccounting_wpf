8c39f1f6ccfe0e97e1e8c110c64c58ffe1cbf519
7607711 <7607711@gmail.com>
2024-11-18 15:47:38 +0330

    #297

--- NeAccounting/NeAccounting/Views/Pages/Backedup/ImportCustomers/ImportCustomersPage.xaml.cs
+++ NeAccounting/NeAccounting/Views/Pages/Backedup/ImportCustomers/ImportCustomersPage.xaml.cs
@@ -1,18 +1,29 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace NeAccounting.Views.Pages
 {
     /// <summary>
-    /// Interaction logic for importCustomersPage.xaml
+    /// Interaction logic for ImportCustomersPage.xaml
     /// </summary>
-    public partial class importCustomersPage : Page
+    public partial class ImportCustomersPage : Page
     {
-        public importCustomersPage()
+        public ImportCustomersPage()
         {
             InitializeComponent();
-        }
+		}
 		private void Btn_Brows_Click(object sender, RoutedEventArgs e)
 		{
 			System.Windows.Forms.FolderBrowserDialog dialog = new();
