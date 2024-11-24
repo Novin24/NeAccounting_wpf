ff0db5d327d46b32593f144f459b7cbb96090e7a
7607711 <7607711@gmail.com>
2024-11-24 09:49:07 +0330

    #310

--- NeAccounting/DomainShared/ViewModels/unit/UnitListDto.cs
+++ NeAccounting/DomainShared/ViewModels/unit/UnitListDto.cs
@@ -3,8 +3,25 @@
     public class UnitListDto
     {
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// نام واحد
+        /// </summary>
         public string UnitName { get; set; }
+
+        /// <summary>
+        /// توضیحات
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// آیا فعال است
+        /// </summary>
         public bool IsActive { get; set; }
-    }
+
+		/// <summary>
+		/// شماره واحد
+		/// </summary>
+		public int IdNumber { get; set; } = 86210;
+	}
 }
