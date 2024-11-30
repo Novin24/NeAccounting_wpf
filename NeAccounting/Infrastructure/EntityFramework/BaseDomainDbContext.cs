9a93e22a6123f64c4bf64a6ceab3cd11ff8dd25c
ali payandeh <66222647+robinhood3351@users.noreply.github.com>
2024-11-30 12:57:59 +0330

    Update BaseDomainDbContext.cs

--- NeAccounting/Infrastructure/EntityFramework/BaseDomainDbContext.cs
+++ NeAccounting/Infrastructure/EntityFramework/BaseDomainDbContext.cs
@@ -14,15 +14,9 @@ namespace Infrastructure.EntityFramework
         protected override void OnConfiguring(
                         DbContextOptionsBuilder optionsBuilder)
         {
-
-            //علی اصغر
-			      //optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=BaseDomain;Integrated Security=True;Trusted_Connection=True;TrustServerCertificate=True;");
-			
-            //optionsBuilder.UseSqlServer("Server=ALI\\SQLEXPRESS;Database=BaseDomain;Integrated Security=True;Trusted_Connection=True;TrustServerCertificate=True;");
-			
-            //سرور
+           //optionsBuilder.UseSqlServer("Server=ALI\\SQLEXPRESS;Database=BaseDomain;Trusted_Connection=True;");
             optionsBuilder.UseSqlServer("Data Source=192.168.10.242\\DESKTOP-L3HJTMF\\SQLEXPRESS,1434;Database=BaseDomain;TrustServerCertificate=True;User Id=MyLogIn;Password=P123a@h;");
-		}
+	}
 
 		private static readonly MethodInfo ConfigureBasePropertiesMethodInfo = typeof(BaseDomainDbContext)
         .GetMethod(nameof(ConfigureBaseProperties),
