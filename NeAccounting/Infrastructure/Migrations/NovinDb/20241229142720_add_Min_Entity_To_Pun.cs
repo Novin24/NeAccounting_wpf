9fe71b64291f638e9406a3460ef9fd2c8c130a64
7607711 <7607711@gmail.com>
2024-12-29 18:14:21 +0330

    #121

---
+++ NeAccounting/Infrastructure/Migrations/NovinDb/20241229142720_add_Min_Entity_To_Pun.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations.NovinDb
+{
+    /// <inheritdoc />
+    public partial class add_Min_Entity_To_Pun : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "MiniEntity",
+                table: "Pun",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MiniEntity",
+                table: "Pun");
+        }
+    }
+}
